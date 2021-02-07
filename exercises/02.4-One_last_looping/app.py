d6044d4e9a015900ee0b3dc4ba8377a1b7271007
nitlara <nitlara@gmail.com>
2021-02-07 15:32:37 +0000

    2.4

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,4 +1,12 @@
 my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 #Your code here:
+my_sample_list[1] = "Steven"
+my_sample_list[-1] = "Pepe"
+my_sample_list[0]="Ruth"+"Pedro"
 
+
+
+
+for i in reversed(my_sample_list):
+    print(i)
